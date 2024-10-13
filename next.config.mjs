25fc1e91cad7c85d3a9e694c2e8515a099c6b327
Juan Manuel Gomez <juanmag.lpez@gmail.com>
2024-10-13 18:26:15 -0500

    fix: next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    output: 'standalone',
     images: {
       domains: ['drive.google.com', 'hebbkx1anhila5yf.public.blob.vercel-storage.com'],
     },
