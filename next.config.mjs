710d364ed3ecc798a4ae40bbe02e5258eea2e2f9
Juan Manuel Gomez <juanmag.lpez@gmail.com>
2024-10-13 18:44:08 -0500

    conf: new version next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,13 @@
+// next.config.mjs (ES module format)
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    output: 'standalone',
     images: {
       domains: ['drive.google.com', 'hebbkx1anhila5yf.public.blob.vercel-storage.com'],
     },
+    output: 'standalone', // If you need standalone output for Vercel
   };
   
   export default nextConfig;
-  
   
