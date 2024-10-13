a5e986c950652b0e2ec747e2029edc2ef08e2dbf
Juan Manuel Gomez <juanmag.lpez@gmail.com>
2024-10-13 17:58:52 -0500

    conf: added reactStrictMode

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
     images: {
       domains: ['drive.google.com', 'hebbkx1anhila5yf.public.blob.vercel-storage.com'],
     },
