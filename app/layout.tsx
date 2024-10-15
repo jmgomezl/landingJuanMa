0c8408491c4989bec6108c279d5a74737ac4baa5
Juan Manuel Gomez <juanmag.lpez@gmail.com>
2024-10-14 22:06:17 -0500

    layout tsx updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,31 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "ICP Colombia",
-  description: "Construyamos el futo",
-};
+  title: 'ICP Colombia',
+  description: 'ICP Colombia',
+  icons: {
+    icon: [
+      {
+        url: 'https://hebbkx1anhila5yf.public.blob.vercel-storage.com/android-chrome-192x192-h04BdRe9rieIY11d5WjtBiiCAw3HW7.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+    ],
+  },
+}
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="es">
+      <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
